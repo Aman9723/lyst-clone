1cd9a097734bcade169c998fcbfff34b479372c4
Aman Singh Chauhan <amansingh9723chauhan@gmail.com>
2022-07-21 01:09:46 +0530

    completed header

---
+++ navbar.js
@@ -0,0 +1,17 @@
+var prev = 0;
+window.onscroll = function () {
+    prev = visible(prev);
+};
+
+function visible(prev) {
+    let cur = window.scrollY;
+    if (cur > prev) {
+        navbar.style.position = 'static';
+    }
+    else {
+        navbar.style.position = 'sticky';
+    }
+    console.log(cur);
+    return cur;
+}
+
